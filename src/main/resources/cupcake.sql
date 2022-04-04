524cd113d98729976f85adc7b0a7d98d241b4ad8
Mark Lundgaard <marklundgaard7@gmail.com>
2022-04-04 12:29:31 +0200

    sql update 2

--- src/main/resources/cupcake.sql
+++ src/main/resources/cupcake.sql
@@ -8,6 +8,7 @@ SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
 -- -----------------------------------------------------
 -- Schema cupcake
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `cupcake`;
 
 -- -----------------------------------------------------
 -- Schema cupcake
@@ -16,6 +17,7 @@ CREATE SCHEMA IF NOT EXISTS `cupcake` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8
 -- -----------------------------------------------------
 -- Schema cupcake
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `cupcake`;
 
 -- -----------------------------------------------------
 -- Schema cupcake
@@ -30,11 +32,14 @@ USE `cupcake`;
 -- -----------------------------------------------------
 -- Table `cupcake`.`user`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `cupcake`.`user`;
+
 CREATE TABLE IF NOT EXISTS `cupcake`.`user`
 (
     `username` VARCHAR(45) NOT NULL,
     `password` VARCHAR(45) NOT NULL,
     `role`     VARCHAR(45) NOT NULL,
+    `balance`  INT         NOT NULL,
     PRIMARY KEY (`username`)
 )
     ENGINE = InnoDB
@@ -45,6 +50,8 @@ CREATE TABLE IF NOT EXISTS `cupcake`.`user`
 -- -----------------------------------------------------
 -- Table `cupcake`.`top`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `cupcake`.`top`;
+
 CREATE TABLE IF NOT EXISTS `cupcake`.`top`
 (
     `top_id`   INT         NOT NULL AUTO_INCREMENT,
@@ -58,6 +65,8 @@ CREATE TABLE IF NOT EXISTS `cupcake`.`top`
 -- -----------------------------------------------------
 -- Table `cupcake`.`bottom`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `cupcake`.`bottom`;
+
 CREATE TABLE IF NOT EXISTS `cupcake`.`bottom`
 (
     `bottom_id`   INT         NOT NULL AUTO_INCREMENT,
@@ -69,17 +78,43 @@ CREATE TABLE IF NOT EXISTS `cupcake`.`bottom`
 
 
 -- -----------------------------------------------------
--- Table `cupcake`.`cupcake`
+-- Table `cupcake`.`order`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cupcake`.`cupcake`
+DROP TABLE IF EXISTS `cupcake`.`order`;
+
+CREATE TABLE IF NOT EXISTS `cupcake`.`order`
 (
-    `bottom_id`  INT NOT NULL,
-    `top_id`     INT NOT NULL,
-    `cupcake_id` INT NOT NULL,
-    `amount`     INT NOT NULL,
+    `order_id`   INT         NOT NULL AUTO_INCREMENT,
+    `username`   VARCHAR(45) NULL,
+    `totalprice` INT         NOT NULL,
+    PRIMARY KEY (`order_id`),
+    INDEX `fk_ordre_user1_idx` (`username` ASC) VISIBLE,
+    CONSTRAINT `fk_ordre_user1`
+        FOREIGN KEY (`username`)
+            REFERENCES `cupcake`.`user` (`username`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`order_line`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cupcake`.`order_line`;
+
+CREATE TABLE IF NOT EXISTS `cupcake`.`order_line`
+(
+    `bottom_id`    INT NOT NULL,
+    `top_id`       INT NOT NULL,
+    `orderline_id` INT NOT NULL,
+    `quantity`     INT NOT NULL,
+    `totalprice`   INT NOT NULL,
+    `order_id`     INT NOT NULL,
     INDEX `fk_bottom_has_top_top1_idx` (`top_id` ASC) VISIBLE,
     INDEX `fk_bottom_has_top_bottom_idx` (`bottom_id` ASC) VISIBLE,
-    PRIMARY KEY (`cupcake_id`),
+    PRIMARY KEY (`orderline_id`),
+    INDEX `fk_order_line_order1_idx` (`order_id` ASC) VISIBLE,
     CONSTRAINT `fk_bottom_has_top_bottom`
         FOREIGN KEY (`bottom_id`)
             REFERENCES `cupcake`.`bottom` (`bottom_id`)
@@ -89,31 +124,10 @@ CREATE TABLE IF NOT EXISTS `cupcake`.`cupcake`
         FOREIGN KEY (`top_id`)
             REFERENCES `cupcake`.`top` (`top_id`)
             ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cupcake`.`order`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cupcake`.`order`
-(
-    `order_id`   INT         NOT NULL AUTO_INCREMENT,
-    `username`   VARCHAR(45) NULL,
-    `cupcake_id` INT         NOT NULL,
-    `totalprice` INT         NOT NULL,
-    PRIMARY KEY (`order_id`),
-    INDEX `fk_ordre_user1_idx` (`username` ASC) VISIBLE,
-    INDEX `fk_order_cupcake1_idx` (`cupcake_id` ASC) VISIBLE,
-    CONSTRAINT `fk_ordre_user1`
-        FOREIGN KEY (`username`)
-            REFERENCES `cupcake`.`user` (`username`)
-            ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT `fk_order_cupcake1`
-        FOREIGN KEY (`cupcake_id`)
-            REFERENCES `cupcake`.`cupcake` (`cupcake_id`)
+    CONSTRAINT `fk_order_line_order1`
+        FOREIGN KEY (`order_id`)
+            REFERENCES `cupcake`.`order` (`order_id`)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION
 )

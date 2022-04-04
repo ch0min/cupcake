a688c1496166b618d473a859ace1f89ab25f0f5f
oTuff <oscartuff@protonmail.com>
2022-04-04 14:04:00 +0200

    added cupcake-values sql script top resources

---
+++ src/main/resources/cupcake-values.sql
@@ -0,0 +1,19 @@
+insert into bottom(bottom_name, price)
+values
+('Chocolate', 5),
+('Vanilla', 5),
+('Nutmeg', 5),
+('Pistacio', 6),
+('Almond', 7);
+
+insert into top(top_name, price)
+values
+('Chocolate', 5),
+('Blueberry', 5),
+('Rasberry', 5),
+('Crispy', 6),
+('Strawberry', 6),
+('Rum/Raisin', 7),
+('Orange', 8),
+('Lemon', 8),
+('Blue cheese',9);

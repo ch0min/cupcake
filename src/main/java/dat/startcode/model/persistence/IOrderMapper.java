e8bbdcad677525817a0b1178fe81c1d34f202531
Mark Lundgaard <marklundgaard7@gmail.com>
2022-04-07 10:27:19 +0200

    Mark commit

--- src/main/java/dat/startcode/model/persistence/IOrderMapper.java
+++ src/main/java/dat/startcode/model/persistence/IOrderMapper.java
@@ -1,5 +1,6 @@
 package dat.startcode.model.persistence;
 
+import dat.startcode.model.entities.Order;
 import dat.startcode.model.entities.OrderLine;
 import dat.startcode.model.exceptions.DatabaseException;
 
@@ -7,5 +8,7 @@ import java.util.List;
 
 public interface IOrderMapper {
     public List<OrderLine> retrieveAllOrderLines() throws DatabaseException;
+
+    public List<Order> retrieveAllUsersAndOrders() throws DatabaseException;
 
 }
